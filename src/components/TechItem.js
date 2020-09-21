77e26328cc873b2ef7b8b57a1de1291515d1c756
Your Name <you@example.com>
2020-09-21 16:48:10 -0300

    chore: Commit inicial

---
+++ src/components/TechItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TechItem({ tech, onDelete }){
+ return(
+  <li key={tech}>
+   {tech}
+   <button onClick={onDelete} type="button">Remover</button>
+  </li>
+ );
+}
+
+export default TechItem;
--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TechItem from './TechItem';
 
 class TechList extends Component {
 
@@ -24,11 +25,17 @@ handleSubmit = e => {
   })
 }
 
+handleDelete = (tech) => {
+ this.setState({ techs: this.state.techs.filter(t => t != tech)})
+}
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
       <ul>
-        {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+        {this.state.techs.map(tech => (
+        <TechItem key={tech} tech={tech} onDelete={() => this.handleDelete(tech)} />
+        ))}
       </ul>
       <input
       type="text"
